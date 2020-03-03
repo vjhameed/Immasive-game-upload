c20ddbf87234fd3e2d3d9480bdf5e6b16e654f9d
hameed <vjhameed3@gmail.com>
2020-03-03 20:58:27 +0500

    complete

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const fileUpload = require("express-fileupload");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
+app.use(
+  fileUpload({
+    // useTempFiles: true,
+    // tempFileDir: "/tmp/"
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
+
+var bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+    parameterLimit: 10000,
+    limit: 1024 * 1024 * 10
+  })
+);
+app.use(
+  bodyParser.json({
+    extended: false,
+    parameterLimit: 10000,
+    limit: 1024 * 1024 * 10
+  })
+);
+
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ limit: "50mb" }));
+
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +55,12 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render("error");
+  console.log(err);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var AdmZip = require("adm-zip");
+const path = require("path");
+var fs = require("fs");
+var uniqid = require("uniqid");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/uploadgame", function(req, res, next) {
+  var zip = new AdmZip(req.files.gamefile.data);
+  const gamefolder = `/games/${uniqid()}`;
+  const gamepath = path.resolve(__dirname, `../public${gamefolder}`);
+  fs.mkdirSync(gamepath);
+  zip.extractAllTo(gamepath);
+  const filepath =
+    req.protocol + "://" + req.get("host") + gamefolder + "/index.html";
+  res.json({ link: filepath });
 });
 
 module.exports = router;
